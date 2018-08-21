cca72de0588bd193cf860dbfdea727b036bc93ce
Brisma M. Arsandi <brismaarsandi@gmail.com>
2018-08-21 19:33:16 +0800

    Add files via upload
    These files are programmed to TTGO modules

---
+++ LoRa-BroadcastNTP.ino
@@ -0,0 +1,117 @@
+#include <SPI.h>
+#include <LoRa.h>
+#include <TimeLib.h>
+#include <NtpClientLib.h>
+#include <Wire.h>
+#include <WiFi.h>
+#include "SSD1306.h"
+//#include "images.h"
+
+#define SCK     5    // GPIO5  -- SX127x's SCK
+#define MISO    19   // GPIO19 -- SX127x's MISO
+#define MOSI    27   // GPIO27 -- SX127x's MOSI
+#define SS      18   // GPIO18 -- SX127x's CS
+#define RST     14   // GPIO14 -- SX127x's RESET
+#define DI00    26   // GPIO26 -- SX127x's IRQ(Interrupt Request)
+
+#define BAND    433E6
+#define PABOOST true
+
+int counter = 0;
+int8_t timeZone = 7;
+int8_t minutesTimeZone = 0;
+bool wifiFirstConnected = false;
+
+const char* ssid = "DANINDRA";
+const char* pass = "30052002";
+
+SSD1306 display(0x3c, 4, 15);
+String rssi = "RSSI --";
+String packSize = "--";
+String packet;
+
+void displayWifiStatus() {
+  display.clear();
+  display.drawString(0, 0, "SSID : ");
+  display.drawString(38, 0, WiFi.SSID());
+
+  IPAddress ip = WiFi.localIP();
+  display.drawString(0, 10, "IP Address : ");
+  display.drawString(60, 10, String(ip));
+
+  long wrssi = WiFi.RSSI();
+  display.drawString(0, 20, "RSSI :");
+  display.drawString(38, 20, String(wrssi));
+  display.drawString(53, 20, " dBm");
+  display.display();
+  delay(1000);
+  display.clear();
+  wifiFirstConnected = true;
+}
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(16,OUTPUT);
+  pinMode(25,OUTPUT);
+  
+  digitalWrite(16, LOW);    // set GPIO16 low to reset OLED
+  delay(50); 
+  digitalWrite(16, HIGH); // while OLED is running, must set GPIO16 in high
+
+  display.init();
+  display.flipScreenVertically();  
+  display.setFont(ArialMT_Plain_10);
+  //delay(1500);
+  WiFi.begin(ssid, pass);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    display.drawString(0, 3, "Connecting . . .");
+    display.display();
+  }
+  display.clear();
+  displayWifiStatus();
+  
+  SPI.begin(SCK,MISO,MOSI,SS);
+  LoRa.setPins(SS,RST,DI00);
+  
+  if (!LoRa.begin(BAND,PABOOST))
+  {
+    display.drawString(0, 0, "Starting LoRa failed!");
+    display.display();
+    while (1);
+  }
+  display.drawString(0, 0, "LoRa Initial success!");
+  display.display();
+  delay(1000);
+
+  NTP.begin ("pool.ntp.org", timeZone, true, minutesTimeZone);
+  NTP.setInterval(63);
+}
+
+void loop() {
+  //displayWifiStatus();
+  if (wifiFirstConnected) {
+      wifiFirstConnected = false;
+      NTP.begin ("pool.ntp.org", timeZone, true, minutesTimeZone);
+      NTP.setInterval (63);
+  }
+  // put your main code here, to run repeatedly:
+  display.clear();
+  display.setTextAlignment(TEXT_ALIGN_LEFT);
+  display.setFont(ArialMT_Plain_10);
+
+  display.drawString( 0, 0, "Sending : ");
+  display.drawString(50, 0, String(counter));
+  display.drawString( 0, 10, NTP.getTimeDateString ());
+  display.display();
+
+  LoRa.beginPacket();
+  LoRa.print(counter);
+  LoRa.print(" ");
+  LoRa.print(NTP.getTimeDateString ());
+  LoRa.endPacket();
+
+  counter++;
+  delay(1000);
+}
---
+++ LoRa-Receiver.ino
@@ -0,0 +1,80 @@
+#include <SPI.h>
+#include <LoRa.h>
+#include <Wire.h>  
+#include "SSD1306.h" 
+#include "images.h"
+
+// Pin definetion of WIFI LoRa 32
+// HelTec AutoMation 2017 support@heltec.cn 
+#define SCK     5    // GPIO5  -- SX127x's SCK
+#define MISO    19   // GPIO19 -- SX127x's MISO
+#define MOSI    27   // GPIO27 -- SX127x's MOSI
+#define SS      18   // GPIO18 -- SX127x's CS
+#define RST     14   // GPIO14 -- SX127x's RESET
+#define DI00    26   // GPIO26 -- SX127x's IRQ(Interrupt Request)
+
+#define BAND    433E6  //you can set band here directly,e.g. 868E6,915E6
+#define PABOOST true
+
+SSD1306 display(0x3c, 4, 15);
+String rssi = "RSSI --";
+String packSize = "--";
+String packet ;
+
+void logo(){
+  display.clear();
+  //display.drawXbm(0,5,logo_width,logo_height,logo_bits);
+  display.display();
+}
+
+void loraData(){
+  display.clear();
+  display.setTextAlignment(TEXT_ALIGN_LEFT);
+  display.setFont(ArialMT_Plain_10);
+  display.drawString(0 , 15 , "Received "+ packSize + " bytes");
+  display.drawStringMaxWidth(0 , 26 , 128, packet);
+  display.drawString(0, 0, rssi);  
+  display.display();
+}
+
+void cbk(int packetSize) {
+  packet ="";
+  packSize = String(packetSize,DEC);
+  for (int i = 0; i < packetSize; i++) { packet += (char) LoRa.read(); }
+  rssi = "RSSI " + String(LoRa.packetRssi(), DEC) ;
+  loraData();
+}
+
+void setup() {
+  pinMode(16,OUTPUT);
+  digitalWrite(16, LOW);    // set GPIO16 low to reset OLED
+  delay(50); 
+  digitalWrite(16, HIGH); // while OLED is running, must set GPIO16 in high、
+  display.init();
+  display.flipScreenVertically();  
+  display.setFont(ArialMT_Plain_10);
+  logo();
+  delay(1500);
+  display.clear();
+  
+  SPI.begin(SCK,MISO,MOSI,SS);
+  LoRa.setPins(SS,RST,DI00);
+  
+  if (!LoRa.begin(BAND,PABOOST)) {
+    display.drawString(0, 0, "Starting LoRa failed!");
+    display.display();
+    while (1);
+  }
+  display.drawString(0, 0, "LoRa Initial success!");
+  display.drawString(0, 10, "Wait for incoming data...");
+  display.display();
+  delay(1000);
+  //LoRa.onReceive(cbk);
+  LoRa.receive();
+}
+
+void loop() {
+  int packetSize = LoRa.parsePacket();
+  if (packetSize) { cbk(packetSize);  }
+  delay(10);
+}
